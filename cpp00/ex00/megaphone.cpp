c83ff308a25f098f94287ee771a9961654d261c9
adgutier <101824703+amsteradri@users.noreply.github.com>
2024-01-09 00:04:46 +0100

    Add files via upload

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adgutier <adgutier@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/06 17:58:43 by adgutier          #+#    #+#             */
+/*   Updated: 2024/01/06 17:58:43 by adgutier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+
+	j = -1;
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (argc > 1 && argv && argv[++j])
+	{
+		i = -1;
+		while (j > 0 && argv[j][++i])
+			std::cout << (char)std::toupper(argv[j][i]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
