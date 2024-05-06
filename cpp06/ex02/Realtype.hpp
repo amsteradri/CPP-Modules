32ee501a1e39ac03ec380e74991cf30b4beb760c
amsteradri <amsteradri@gmail.com>
2024-05-06 17:45:46 +0200

    finish 02 cpp06

---
+++ cpp06/ex02/Realtype.hpp
@@ -0,0 +1,26 @@
+#pragma once 
+
+#include <stdint.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <iomanip> 
+
+class Base{
+    public: 
+        virtual ~Base() {}
+};
+
+class A : public Base {
+
+};
+
+class B : public Base {
+    
+};
+
+class C : public Base {
+
+};
+
