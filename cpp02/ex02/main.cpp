d37832437318d170a5b393b427276ae5eafe6cb1
amsteradri <amsteradri@gmail.com>
2024-06-17 18:53:39 +0200

    revisando y fixeando

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: adgutier <adgutier@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 11:49:28 by adgutier          #+#    #+#             */
-/*   Updated: 2024/06/17 18:51:07 by adgutier         ###   ########.fr       */
+/*   Updated: 2024/06/17 18:53:06 by adgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main( void )
 {
 	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed const b( Fixed( 5 ) * Fixed( 2 ) );
 
 	std::cout << "a: " << a << std::endl;
 	std::cout << "++a: " << ++a << std::endl;
